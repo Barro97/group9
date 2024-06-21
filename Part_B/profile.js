e6cc1d434661018d602003d06bc4f34d174a1bcb
Barro97 <rosenthb@post.bgu.ac.il>
2024-06-21 17:31:26 +0300

    better profile

---
+++ Part_B/profile.js
@@ -0,0 +1,23 @@
+"use strict";
+const user = JSON.parse(localStorage.getItem("activeUser"));
+const followButton = document.getElementById("follow-button");
+const followCount = document.querySelector(".follow-count");
+let isFollowing = false;
+document.addEventListener("DOMContentLoaded", function () {
+  followButton.addEventListener("click", function () {
+    console.log(isFollowing);
+    if (isFollowing) {
+      // UnFollow logic
+      isFollowing = false;
+      followButton.textContent = "+ Follow";
+      followButton.classList.remove("pressed");
+      followCount.textContent = `Followers: 0`;
+    } else {
+      // Follow logic
+      isFollowing = true;
+      followButton.textContent = "Following";
+      followButton.classList.add("pressed");
+      followCount.textContent = `Followers: 1`;
+    }
+  });
+});
