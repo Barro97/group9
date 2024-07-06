b61d8c34697581232c0d0135ac6b75f88b73efc7
barro97 <rosenthb@post.bgu.ac.il>
2024-07-06 16:46:06 +0300

    working log in

--- Part_C/app.py
+++ Part_C/app.py
@@ -1,55 +1,67 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
-import pymongo
 
 from flask import *
+
 app = Flask(__name__)
+app.secret_key = '123'
 
 from pages.aboutus.aboutus import about_us
+
 app.register_blueprint(about_us)
 
 from pages.login.login import login
+
 app.register_blueprint(login)
 
 from pages.chat.chat import chat
+
 app.register_blueprint(chat)
 
 from pages.home.home import home
+
 app.register_blueprint(home)
 
 from pages.myprofile.my_profile import my_profile
+
 app.register_blueprint(my_profile)
 
 from pages.notifications.notifications import notifications
+
 app.register_blueprint(notifications)
 
 from pages.organization.organization import organization
+
 app.register_blueprint(organization)
 
 from pages.privacypolicy.privacy_policy import privacy_policy
+
 app.register_blueprint(privacy_policy)
 
 from pages.profile.profile import profile
+
 app.register_blueprint(profile)
 
 from pages.project.project import project
+
 app.register_blueprint(project)
 
 from pages.searchresults.search_results import search_results
+
 app.register_blueprint(search_results)
 
 from pages.signup.signup import signup
-app.register_blueprint(signup)
 
+app.register_blueprint(signup)
 
 
 @app.route('/')
 def home():
     return render_template('login.html')
 
+
 if __name__ == '__main__':
     app.run(debug=True)
-
 
 uri = "mongodb+srv://rinak:SbSaxSwP6TEHmWGw@workfolio.w1hkpdf.mongodb.net/?retryWrites=true&w=majority&appName=Workfolio"
 myclient = MongoClient(uri, server_api=ServerApi('1'))
@@ -60,6 +72,4 @@ users_collection = mydb['users']
 
 @app.route('/MongoDB')
 def MongoDB_func():
-    name='Rina'
-
-
+    name = 'Rina'
--- Part_C/pages/login/login.py
+++ Part_C/pages/login/login.py
@@ -1,7 +1,9 @@
+# from urllib import request
+
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 import pymongo
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for, jsonify, session
 
 # about blueprint definition
 login = Blueprint(
@@ -18,7 +20,27 @@ myclient = MongoClient(uri, server_api=ServerApi('1'))
 mydb = myclient['user_database']
 users_collection = mydb['users']
 
+
 # Routes
 @login.route('/login')
 def index():
     return render_template('login.html')
+
+
+@login.route('/check_user', methods=['GET', 'POST'])  # The route for verifying the user
+def check_user():
+    if request.method == 'POST':
+        data = request.json  # Get the form data from the JS
+        email = data.get('email')
+        password = data.get('password')
+
+        query = {"email": email, "password": password}
+        user = users_collection.find_one(query)  # Try to find the user that was input in the DB
+        if user:
+            session['user'] = user  # Save the current user
+            user.pop('_id', None)
+            session['logged_in'] = True  # A boolean for page interaction
+            return jsonify({"success": True, "redirect": "/home"})  # A JSON for successful log in attempt
+        else:
+            return jsonify(
+                {"success": False, "message": "Invalid email or password."})  # A JSON for failed log in attempt
