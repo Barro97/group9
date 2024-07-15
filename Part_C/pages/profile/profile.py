7e6cf9ae0bacea8c86c7599be03daa244856c183
barro97 <rosenthb@post.bgu.ac.il>
2024-07-15 11:53:48 +0300

    follow functionality works

--- Part_C/pages/profile/profile.py
+++ Part_C/pages/profile/profile.py
@@ -17,16 +17,43 @@ myclient = MongoClient(uri, server_api=ServerApi('1'))
 mydb = myclient['user_database']
 users_collection = mydb['users']
 project_collection = mydb['projects']
+followers_collection = mydb['followers']
+profile_owner = ''
+
 
 @profile.route('/profile/<user_email>')
 def view_profile(user_email):
-    print(user_email,session.get('user')['email'])
+    print(user_email, session.get('user')['email'])
     if session.get('user')['email'] == user_email:
         return redirect(url_for('my_profile.index'))
     else:
         user = users_collection.find_one({'email': user_email})
         if user:
+            session['profile_owner'] = user_email
             projects = project_collection.find({'owner': user_email})
-            return render_template('profile.html', user=user, projects=projects)
+            count = followers_collection.count_documents({'followee': session.get('profile_owner')})
+            return render_template('profile.html', user=user, projects=projects, count=count)
         else:
             return "User not found", 404
+
+
+@profile.route('/follow')
+def follow():
+    followers_collection.insert_one({'follower': session.get('user')['email'], 'followee': session.get('profile_owner')})
+    count = followers_collection.count_documents({'followee': session.get('profile_owner')})
+    return jsonify({'status': 'success', 'count': count})
+
+
+@profile.route('/unfollow')
+def unfollow():
+    followers_collection.delete_one({'follower': session.get('user')['email'], 'followee': session.get('profile_owner')})
+    return jsonify({'status': 'success'})
+
+
+@profile.route('/check_for_following')
+def check_for_following():
+    isFollowing = followers_collection.find_one({'follower': session.get('user')['email'], 'followee': session.get('profile_owner')})
+    if isFollowing:
+        return jsonify({'isFollowing': True})
+    else:
+        return jsonify({'isFollowing': False})
